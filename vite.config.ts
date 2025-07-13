648bbb7c38835ed648f4ee3fbe3b7ad2968f097c
Dunn, Simeon <simedunn01@gmail.com>
2025-07-13 13:13:30 -0400

    remove wrangler dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import tailwindcss from "@tailwindcss/vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
 import Icons from "unplugin-icons/vite";
 
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss(), cloudflare(), Icons()],
+  plugins: [vue(), vueDevTools(), tailwindcss(), Icons()],
   resolve: {
     alias: {
       "~": fileURLToPath(new URL("./src", import.meta.url)),

8e680fd603dc039959ff395701371a0c584665e2
Dunn, Simeon <simedunn01@gmail.com>
2025-07-13 13:10:17 -0400

    deploy to cloudflare pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import tailwindcss from "@tailwindcss/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import Icons from "unplugin-icons/vite";
 
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss(), Icons()],
+  plugins: [vue(), vueDevTools(), tailwindcss(), cloudflare(), Icons()],
   resolve: {
     alias: {
       "~": fileURLToPath(new URL("./src", import.meta.url)),

47ffe0af5b625d244d78d81f39f7e5b4d6683499
Dunn, Simeon <simedunn01@gmail.com>
2025-07-13 19:19:45 -0400

    vite-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,54 @@ import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import tailwindcss from "@tailwindcss/vite";
 import Icons from "unplugin-icons/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss(), Icons()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    tailwindcss(),
+    Icons(),
+    VitePWA({
+      injectRegister: "auto",
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}']
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg", "favicon.svg"],
+      manifest: {
+        name: 'Tribes App',
+        short_name: 'Tribes',
+        description: 'Track goal progression with your tribe',
+        theme_color: '#FFFFFF',
+        background_color: '#FFFFFF',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "~": fileURLToPath(new URL("./src", import.meta.url)),
